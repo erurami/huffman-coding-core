e820eb48d95391aa3cf2e02242e30f2017d5954f
erurami <88187787+erurami@users.noreply.github.com>
2021-12-31 18:56:21 +0700

    bar

--- test.cpp
+++ test.cpp
@@ -8,13 +8,44 @@ int main()
     FILE* p_file1;
     FILE* p_file2;
 
-    p_file1 = fopen("test.txt"     , "rb");
-    p_file2 = fopen("test.txt.ehuf", "wb");
+    if (fopen_s(&p_file1, "test.txt"     , "rb") != 0)
+    {
+        printf("error loading test.txt\n");
+        return -1;
+    }
+
+    if (fopen_s(&p_file2, "test.txt.ehuf", "wb") != 0)
+    {
+        printf("error loading test.txt.ehuf\n");
+        return -1;
+    }
 
     Compress(p_file1, p_file2);
 
     fclose(p_file1);
     fclose(p_file2);
+
+
+
+    if (fopen_s(&p_file1, "test1.txt"     , "rb") != 0)
+    {
+        printf("error loading test1.txt\n");
+        return -1;
+    }
+    if (fopen_s(&p_file2, "test1.txt.ehuf", "wb") != 0)
+    {
+        printf("error loading test1.txt.ehuf\n");
+        return -1;
+    }
+
+    Compress(p_file1, p_file2);
+
+    fclose(p_file1);
+    fclose(p_file2);
+
+    printf("a\n");
+
+    printf("ê≥èÌèIóπ\n");
 
     return 0;
 }
